63ef040d492cbf44d664320a0b6f4f4a914b8019
Maikito99XD <msteelmarin@gmail.com>
2022-10-11 00:46:54 +0200

    first commit

---
+++ 2DAW/Entorno_Servidor/Hola_mundo.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<body>
+	<head>
+		<title>Hello world</title
+	</head>
+	</body>
+		<?php
+			<!--- variables --->
+			$var= "6";
+			
+			echo "Hello world";
+			echo "$var";
+		?>
+	</body
+</html
