7374d64795b5b4a0b39718cb742a74ca4b19e8cd
Maikito99XD <msteelmarin@gmail.com>
2022-10-12 23:33:35 +0200

    update 12/10/22

--- Entorno_Servidor/Act_10_ClaseVehiculo/Coche.php
+++ Entorno_Servidor/Act_10_ClaseVehiculo/Coche.php
@@ -2,16 +2,47 @@
     class Coche extends Cuatro_ruedas{
         private int $numero_cadenas_nieve;
 
-        function __construct($numero_cadenas_nieve){
+        function __construct(string $color,$peso,int $numero_puertas,int $numero_cadenas_nieve){
+            parent::__construct($color,$peso,$numero_puertas);
             $this->numero_cadenas_nieve = $numero_cadenas_nieve;
         }
 
-        public function anyadir_cadenas_nieve(){
-
+        public function anyadir_cadenas_nieve(int $cantidaAnyadir){
+            $this->numero_cadenas_nieve = $this->numero_cadenas_nieve + $cantidaAnyadir;
         }
 
-        public function quitar_cadenas_nieve(){
+        public function quitar_cadenas_nieve(int $cantidadEliminar){
+            $aux = $this->numero_cadenas_nieve - $cantidadEliminar;
+            if($aux >= 0){
+                $this->numero_cadenas_nieve = $aux;
+            }else{
+                echo "HAS ELIMINADO DEMASIADAS CADENAS, PON UN NUMERO MÁS BAJO DE: $this->numero_cadenas_nieve";
+            }
+        }
 
+        public function getCadenasNieve(){
+            return $this->numero_cadenas_nieve;
+        }
+
+        public function setCadenasNieve(int $modificaCadenasNieve){
+            if($modificaCadenasNieve < 0){
+                echo "NO PUEDES PONER UN NUMERO NEGATIVO!";
+            }else{
+                $this->numero_cadenas_nieve = $modificaCadenasNieve;
+                return $this->numero_cadenas_nieve;
+            }
+        }
+
+        public function anyadir_persona($pesoPersona){
+            if($this->getPeso()>= 1500 || $this->numero_cadenas_nieve <= 2){
+                echo "Atención, ponga 4 cadenas para la nieve.<br>";
+            }
+        }
+
+        public function __toString(){
+            return self::class . ": ".$this->getColor().", ".$this->getPeso()."kg, ".
+                $this->getPuertas()." puertas, ".$this->numero_cadenas_nieve." cadenas para la nieve, ".
+                $this->getNumeroCambiosColor()." veces se ha cambiado el color<br>";
         }
     }
 
--- Entorno_Servidor/Act_10_ClaseVehiculo/Vehiculo.php
+++ Entorno_Servidor/Act_10_ClaseVehiculo/Vehiculo.php
@@ -1,7 +1,8 @@
 <?php
-    class Vehiculo{
+    abstract class Vehiculo{
         private string $color;
         private $peso;
+        protected static $numero_cambio_color = 0;
 
         //Creación del constructor de la clase Vehiculo
         function __construct(string $color, $peso){
@@ -20,7 +21,11 @@
             echo "El vehiculo esta circulando";
         }
         
+        public static function ver_atributo(Object $obj){
+            return $obj->__toString();
+        }
         //Creación y definir que por cada persona se añada el peso de la misma al peso del vehiculo
+        /*
         public function anyadir_persona($pesoPersona){
             if(is_float($pesoPersona) || is_integer($pesoPersona)){
                 $this->peso = $this->peso + $pesoPersona;
@@ -28,22 +33,37 @@
                 echo "EL PESO DE LA PERSONA TIENE QUE SER UN ENTERO O CON DECIMALES!";
             }
         }
+        */
+
+
+        //Añadir persona con abstract
+        public abstract function anyadir_persona($pesoPersona);
 
         //Creación de getters y setters
-        private function getColor(){
+        public function getColor(){
             return $this->color;
         }
 
-        private function getPeso(){
+        public function getPeso(){
             return $this->peso;
         }
 
-        private function setColor($color){
-            $this->color = $color;
+        public function getNumeroCambiosColor(){
+            return self::$numero_cambio_color;
         }
 
-        private function setPeso($peso){
-            $this->peso = $peso;
+        public function setColor($color){
+            $this->color = $color;
+            self::$numero_cambio_color++;
+        }
+
+        public function setPeso($peso){
+            if($peso > 2100){
+                return "El peso maximo permitido es de 2100kg!";
+            }else{
+                $this->peso = $peso;
+                return $this->peso;
+            }
         }
     }
 ?>
