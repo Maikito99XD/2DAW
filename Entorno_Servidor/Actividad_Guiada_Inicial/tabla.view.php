e415045cba528f211e6e286fc1bdf8579e82c479
Maikito99XD <msteelmarin@gmail.com>
2022-11-15 19:17:55 +0100

    update 15/11/22

---
+++ Entorno_Servidor/Actividad_Guiada_Inicial/tabla.view.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <title>Consultas</title>
+    <meta name="description" content="PHP">
+    <meta name="author" content="Mike">
+    <style>
+		label, input{display: block;}
+	</style>
+</head>
+<body>
+    <h2>Ejercicio <?=$Ej?></h2>
+    <table border="1">
+        <thead>
+            <tr>
+                <?php foreach ($filas[0] as $key => $dato) : ?>
+                    <th><?=$key?></th>
+                <?php endforeach; ?>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($filas as $fila) : ?>
+                <tr>
+                    <?php foreach ($fila as $dato) : ?>
+                        <td><?=$dato?></td>
+                    <?php endforeach; ?>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+
+
+
+</body>
+</html>
